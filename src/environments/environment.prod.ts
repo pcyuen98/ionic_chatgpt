1d00cb047f08dbbca33ef0cda7307e3a3809a016
pcyuen98 <pcyuen98@gmail.com>
2023-05-24 21:32:50 +0800

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  OPENAI_API_KEY: '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', //S2qXjVvTVF2aLjfeyYgVT3BlbkFJi8P6s2ZTx68tAzKljlUg
+  OPENAI_API_KEY: 'key'
 
-  // S2qXjVvTVF2aLjfeyYgVT3BlbkFJi8P6s2ZTx68tAzKljlUg own
-  // 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    friend
 };
